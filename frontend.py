f68ff15d601480bf792e493a26a4ea3c42a298c6
Jaykorade <jaykorade@gmail.com>
2025-08-21 20:15:42 +0530

    mychatbot

--- frontend.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-user_input = st.chat_input("Type here")
-if user_input:
-    with st.chat_message('user'):
-        st.text(user_input)
-    with st.chat_message('AI'):
-            st.text(user_input)
